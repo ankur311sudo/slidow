c21da72d86c128d5c660fca8cd48ff0bdc04e3ae
ankur311sudo <ankur311275@gmail.com>
2026-01-19 23:44:23 +0530

    neovim  updates

--- neovim.lua
+++ neovim.lua
@@ -1,43 +1,56 @@
+-- return {
+-- 	{
+-- 		"LazyVim/LazyVim",
+-- 		opts = {
+-- 			colorscheme = "tokyodark",
+-- 		},
+-- 	},
+--
+-- 	{
+-- 		"tiagovla/tokyodark.nvim",
+-- 		priority = 1000,
+-- 		opts = {
+-- 			-- keep empty or add options later
+-- 		},
+-- 		config = function(_, opts)
+-- 			require("tokyodark").setup(opts)
+-- 			vim.cmd([[colorscheme tokyodark]])
+--
+-- 			-- ================================
+-- 			-- FIX TABLINE CURRENT BUFFER
+-- 			-- ================================
+-- 			local hl = vim.api.nvim_set_hl
+--
+-- 			-- Active tab / buffer
+-- 			hl(0, "TabLineSel", {
+-- 				fg = "#11121D", -- background color
+-- 				bg = "#7AA2F7", -- blue accent
+-- 				bold = true,
+-- 			})
+--
+-- 			-- Inactive tabs
+-- 			hl(0, "TabLine", {
+-- 				fg = "#787C99",
+-- 				bg = "#11121D",
+-- 			})
+--
+-- 			-- Fill area
+-- 			hl(0, "TabLineFill", {
+-- 				bg = "#11121D",
+-- 			})
+-- 		end,
+-- 	},
+-- }
+
 return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "tokyodark",
+			colorscheme = "tokyonight-night",
 		},
-	},
-
-	{
-		"tiagovla/tokyodark.nvim",
-		priority = 1000,
-		opts = {
-			-- keep empty or add options later
-		},
-		config = function(_, opts)
-			require("tokyodark").setup(opts)
-			vim.cmd([[colorscheme tokyodark]])
-
-			-- ================================
-			-- FIX TABLINE CURRENT BUFFER
-			-- ================================
-			local hl = vim.api.nvim_set_hl
-
-			-- Active tab / buffer
-			hl(0, "TabLineSel", {
-				fg = "#11121D", -- background color
-				bg = "#7AA2F7", -- blue accent
-				bold = true,
-			})
-
-			-- Inactive tabs
-			hl(0, "TabLine", {
-				fg = "#787C99",
-				bg = "#11121D",
-			})
-
-			-- Fill area
-			hl(0, "TabLineFill", {
-				bg = "#11121D",
-			})
-		end,
 	},
 }
